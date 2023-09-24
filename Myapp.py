c2fb96e880fb37ce6d45d0b152d2acfc48b27139
Wang <813528152@qq.com>
2023-09-24 16:45:42 +0800

    feat(Myapp):增加运算类,实现生成式子

--- Myapp.py
+++ Myapp.py
@@ -1,6 +1,25 @@
 import argparse
 
 
+class Operation:
+
+    def __init__(self):
+        self.number = 10
+        self.value = 10
+
+    def operation(self):
+        f_exercise = open('./Exercise.txt', 'a+', encoding='utf-8')
+        f_answer = open('./Answer.txt', 'a+', encoding='utf-8')
+        f_exercise.seek(0)
+        f_answer.seek(0)
+        f_exercise.truncate()
+        f_answer.truncate()
+        count = 0
+        # TODO
+        f_exercise.close()
+        f_answer.close()
+
+
 def add_parm():
     argument_parser = argparse.ArgumentParser()
     argument_parser.add_argument("-n", dest="sum", help="生成数量")
@@ -15,7 +34,10 @@ def main():
     args = add_parm()
     # 支持操作：-n-r输入 或 -e-a输入 两种情况。
     if args.range and args.sum:
-        print("-n-r")
+        opera = Operation()
+        opera.number = int(args.sum)
+        opera.value = int(args.range)
+        opera.operation()
     elif args.exercise_file and args.answer_file:
         print("-e-a")
     else:
